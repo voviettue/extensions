860cf723a34bd88d30cf379bd0e8c38441792601
Thien Nguyen <thien@catex.se>
2022-05-16 17:50:45 +0700

    [Release] Fix script build

--- scripts/build.js
+++ scripts/build.js
@@ -5,7 +5,7 @@ const packageJson = require('../package.json')
 packageJson.workspaces.forEach(workspaceDir => {
   glob(workspaceDir + '/dist/index.js', null, function (err, files) {
     files.forEach(filepath => {
-      const destPath = './dist/' + filepath.replace('/dist', '').replace('/packages')
+      const destPath = './dist' + filepath.replace(/^packages|\/dist/g, '')
       fs.mkdirSync(destPath.replace('/index.js', ''), { recursive: true })
       fs.copyFile(filepath, destPath, err => { })
     });
