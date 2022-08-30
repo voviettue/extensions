ab5eb145928863adb1fb234bda228830b0e0a887
DaiLoc5698 <76195735+DaiLoc5698@users.noreply.github.com>
2022-08-30 15:01:02 +0700

    [Query] update api query

---
+++ packages/front-office/src/endpoints/controllers/queries.ts
@@ -0,0 +1,163 @@
+import axios from 'axios';
+import { ApiExtensionContext } from '@directus/shared/types';
+import { BaseException } from '@directus/shared/exceptions';
+
+async function deleteLogs(req: any, res: any, ctx: ApiExtensionContext) {
+	try {
+		const { ItemsService } = ctx.services;
+
+		const activityService = new ItemsService('directus_activity', {
+			schema: req.schema,
+			accountability: req.accountability,
+		});
+
+		await activityService.deleteByQuery({
+			filter: {
+				item: { _eq: req.params.id },
+				action: { _eq: 'execute' },
+			},
+		});
+	} catch (e: any) {
+		return new BaseException(e.message, e.status, e?.code);
+	}
+}
+
+async function queryExecute(req: any, res: any, ctx: ApiExtensionContext) {
+	const { ItemsService } = ctx.services;
+
+	const queryItemsService = new ItemsService('cms_queries', {
+		schema: req.schema,
+		accountability: req.accountability,
+	});
+
+	const activityService = new ItemsService('directus_activity', {
+		schema: req.schema,
+		accountability: req.accountability,
+	});
+
+	let query = null;
+	let error = null;
+
+	try {
+		query = await queryItemsService.readOne(req.params.id, { fields: '*' });
+		let data = null;
+
+		switch (query.query) {
+			case 'items':
+				data = await executeQueryItems(req, query, ctx);
+				break;
+			case 'api':
+				data = await executeQueryApi(query, ctx);
+				break;
+			default:
+				break;
+		}
+
+		if (data) {
+			await Promise.all([
+				queryItemsService.updateOne(query.id, { output: data }),
+				activityService.createOne({
+					action: 'execute',
+					user: req.accountability!.user,
+					collection: query?.options?.collection || '',
+					ip: req.accountability!.ip,
+					user_agent: req.accountability!.userAgent,
+					item: req.params.id,
+					comment: {
+						message: `[${query.name}] Execution started from user request`,
+						data: data,
+						type: query?.query,
+						name: query?.name,
+					},
+				}),
+			]);
+		}
+
+		return { data: data };
+	} catch (e: any) {
+		error = e;
+	}
+
+	await activityService.createOne({
+		action: 'execute',
+		user: req.accountability!.user,
+		collection: query?.options?.collection || '',
+		ip: req.accountability!.ip,
+		user_agent: req.accountability!.userAgent,
+		item: req.params.id,
+		comment: {
+			message: `[${query?.name}] Execution failed with status ${error.status} ${error.code}`,
+			erorr: error,
+			type: query?.query,
+			name: query?.name,
+		},
+	});
+
+	return new BaseException(error.message, error.status, error?.code);
+}
+
+async function executeQueryApi(query: any, ctx: ApiExtensionContext) {
+	const { InvalidPayloadException } = ctx.exceptions;
+
+	try {
+		if (!query.options?.method || !query.options?.url) {
+			throw new InvalidPayloadException('Invalid params');
+		}
+
+		const headers: Record<string, any> = {};
+		query.options?.headers?.map((e: any) => (headers[e.key] = e.value));
+
+		const params: Record<string, any> = {};
+		query.options?.query?.map((e: any) => (params[e.key] = e.value));
+
+		const body = query.options?.request_body;
+
+		let options = {};
+		if (query.options.method !== 'delete') {
+			options = {
+				method: query.options.method,
+				url: query.options.url,
+				params: params,
+				data: body,
+				headers: headers,
+				timeout: query.timeout,
+			};
+		}
+
+		const { data } = await axios(options);
+		return data;
+	} catch (e: any) {
+		throw new InvalidPayloadException(e?.message);
+	}
+}
+
+async function executeQueryItems(req: any, query: any, ctx: ApiExtensionContext) {
+	const { InvalidPayloadException } = ctx.exceptions;
+	const { ItemsService } = ctx.services;
+
+	try {
+		if (!query?.options?.collection || !query?.options?.fields) {
+			throw new InvalidPayloadException('invalid params');
+		}
+
+		const itemsService = new ItemsService(query.options.collection, {
+			schema: req.schema,
+			accountability: req.accountability,
+		});
+
+		const data = await itemsService.readByQuery({
+			filter: query.options?.filter,
+			fields: query.options.fields,
+			limit: query.options?.per_page || 20,
+		});
+
+		return data;
+	} catch (e: any) {
+		throw new InvalidPayloadException(e?.message);
+	}
+}
+
+export default {
+	queryExecute,
+	deleteLogs,
+};
--- packages/front-office/src/endpoints/queries/index.ts
+++ packages/front-office/src/endpoints/queries/index.ts
@@ -1,157 +1,27 @@
-import axios from 'axios';
 import { defineEndpoint } from '@directus/extensions-sdk';
-import { Accountability, SchemaOverview } from '@directus/shared/types';
-import { Action } from '@directus/shared/types';
+import queryController from '../controllers/queries';
 
 export default defineEndpoint({
 	id: 'front-office/queries',
-	handler: async (router, { services, exceptions }) => {
-		const { ItemsService } = services;
-		const { InvalidPayloadException } = exceptions;
-
+	handler: async (router, ctx) => {
 		router.patch('/:id/execute', async (req: any, res: any) => {
-			const queryItemsService = new ItemsService('cms_queries', {
-				schema: req.schema,
-				accountability: req.accountability,
-			});
-
-			const activityService = new ItemsService('directus_activity', {
-				schema: req.schema,
-				accountability: req.accountability,
-			});
-
-			let query = null;
-			let error = null;
-
 			try {
-				query = await queryItemsService.readOne(req.params.id, { fields: '*' });
-				let result = null;
+				const data = await queryController.queryExecute(req, res, ctx);
 
-				switch (query.query) {
-					case 'items':
-						result = await itemsQueryExecute(query, req.schema, req.accountability);
-						break;
-					case 'api':
-						result = await apiQueryExecute(query);
-						break;
-					default:
-						break;
-				}
-
-				if (result) {
-					await Promise.all([
-						queryItemsService.updateOne(query.id, { output: result }),
-						activityService.createOne({
-							action: 'execute',
-							user: req.accountability!.user,
-							collection: query?.options?.collection || '',
-							ip: req.accountability!.ip,
-							user_agent: req.accountability!.userAgent,
-							item: req.params.id,
-							comment: {
-								message: `[${query.name}] Execution started from user request`,
-								data: result,
-								type: query?.query,
-								name: query?.name,
-							},
-						}),
-					]);
-				}
-
-				return res.json({ data: result });
-			} catch (e: any) {
-				error = e;
-			}
-
-			await activityService.createOne({
-				action: 'execute',
-				user: req.accountability!.user,
-				collection: query?.options?.collection || '',
-				ip: req.accountability!.ip,
-				user_agent: req.accountability!.userAgent,
-				item: req.params.id,
-				comment: {
-					message: `[${query?.name}] Execution failed with status ${error.status} ${error.code}`,
-					erorr: error,
-					type: query?.query,
-					name: query?.name,
-				},
-			});
-
-			return res.status(error.status).json({ status: error.status, message: error.message });
-		});
-
-		router.get('/test', async (req: any, res: any) => {
-			res.json({ data: 'OK1' });
-		});
-
-		router.post('/test', async (req: any, res: any) => {
-			res.json({ data: 'OK2' });
-		});
-
-		router.put('/test', async (req: any, res: any) => {
-			res.json({ data: 'OK3' });
-		});
-
-		router.patch('/test', async (req: any, res: any) => {
-			res.json({ data: 'OK4' });
-		});
-
-		router.delete('/test', async (req: any, res: any) => {
-			res.json({ data: 'OK5' });
-		});
-
-		async function apiQueryExecute(query: any) {
-			try {
-				if (!query.options?.method || !query.options?.url) {
-					throw new InvalidPayloadException('Invalid params');
-				}
-
-				// -TODO
-				const headers = query.options?.headers.reduce((acc: any, { key, value }) => ({ ...acc, [key]: value }), {});
-				const params = query.options?.query.reduce((acc: any, { key, value }) => ({ ...acc, [key]: value }), {});
-				const body = query.options?.request_body;
-
-				let options = {};
-				if (query.options.method !== 'delete') {
-					options = {
-						method: query.options.method,
-						url: query.options.url,
-						params: params,
-						data: body,
-						headers: headers,
-						timeout: query.timeout,
-					};
-				}
-
-				const { data } = await axios(options);
-				return data;
+				return res.json({ data: data });
 			} catch (error: any) {
-				throw new InvalidPayloadException(error?.message);
+				return res.status(error.status).json({ status: error.status, message: error.message });
 			}
-		}
+		});
 
-		async function itemsQueryExecute(query: any, schema: SchemaOverview, accountability: Accountability) {
+		router.delete('/:id/logs', async (req: any, res: any) => {
 			try {
-				if (!query?.options?.collection || !query?.options?.fields) {
-					throw new InvalidPayloadException('invalid params');
-				}
+				await queryController.deleteLogs(req, res, ctx);
 
-				const itemsService = new ItemsService(query.options.collection, {
-					schema: schema,
-					accountability: accountability,
-				});
-
-				const data = await itemsService.readByQuery({
-					filter: query.options?.filter,
-					fields: query.options.fields,
-					limit: query.options?.per_page || 20,
-				});
-
-				return data;
+				res.json();
 			} catch (error: any) {
-				throw new InvalidPayloadException(error?.message);
+				return res.status(error.status).json({ status: error.status, message: error.message });
 			}
-		}
+		});
 	},
 });
