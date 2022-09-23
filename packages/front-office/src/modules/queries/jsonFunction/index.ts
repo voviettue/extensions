34e0b3d0308b07322903a06e551e3dee08eae147
Đại Lộc Lê Quang <dailoc.lq@gmail.com>
2022-09-23 16:00:29 +0700

    [Query] query js object function, add data default

--- packages/front-office/src/modules/queries/jsonFunction/index.ts
+++ packages/front-office/src/modules/queries/jsonFunction/index.ts
@@ -36,11 +36,17 @@ export default defineQuery({
 				interface: 'input-code',
 				required: true,
 				placeholder: 'Enter code here...',
+				options: {
+					language: 'javascript',
+					lineNumber: true,
+					template:
+						'{\n\t"object": "enter value...",\n\tmyFun1: () => {\n\t\treturn "something..."\n\t},\n\tmyFun2: (param) => {\n\t\treturn param\n\t},\n    myFun3: async () => {\n\t\tconst data = fetch("enter url...")\n        .then(response => response.json())\n\t\t.then(data => data);\n\t\treturn data;\n\t},\n}',
+				},
 			},
 		},
 	],
 	beforeSave: (values) => {
-		if (values?.options?.json) {
+		if (values?.options?.code) {
 			values.output = values?.options?.code;
 			values.refresh_on_load = false;
 		}
