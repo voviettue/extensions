349744d4a3163bc42f13f6353e3c2f62be4c9773
Huy Bui <huy@catex.se>
2022-10-12 20:36:25 +0700

    [Display] Add button, duration and icon button

--- packages/front-office/src/modules/displays/index.ts
+++ packages/front-office/src/modules/displays/index.ts
@@ -4,5 +4,8 @@ import link from './link';
 import number from './number';
 import percentage from './percentage';
 import text from './text';
+import label from './label';
+import duration from './duration';
+import iconButton from './icon-button';
 
-export default [button, date, link, number, percentage, text];
+export default [button, date, link, number, percentage, text, label, duration, iconButton];
