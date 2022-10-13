ee91f1d3b5f10271bbfa8c2a6b2dc23c1b2e8c17
Lộc Lê <loc@catex.se>
2022-10-13 13:47:46 +0700

    [Widget][Calendar] update parse-date

--- packages/front-office/src/modules/utils/parse-date.ts
+++ packages/front-office/src/modules/utils/parse-date.ts
@@ -2,18 +2,20 @@ import isValid from 'date-fns/isValid';
 import parse from 'date-fns/parse';
 
 export default function (value: any) {
-	if (typeof value !== 'string') return null;
+	if (typeof value !== 'string' && typeof value !== 'number') return null;
 
 	const formats = ["yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", "yyyy-MM-dd'T'HH:mm:ss", 'yyyy-MM-dd'];
-	let date = null;
 
-	for (const format of formats) {
-		const parseDate = parse(value, format, new Date());
-		if (isValid(parseDate)) {
-			date = parseDate;
-			break;
+	if (typeof value === 'string') {
+		for (const format of formats) {
+			const parseDate = parse(value, format, new Date());
+			if (isValid(parseDate)) return parseDate;
 		}
 	}
 
-	return date;
+	if (String(value).length >= 8 && isValid(new Date(value))) {
+		return new Date(value);
+	}
+
+	return null;
 }
